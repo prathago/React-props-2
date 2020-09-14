ab31d89fa23c0ffd3ad7bec93e69ea1328358edf
prathago <65850199+prathago@users.noreply.github.com>
2020-09-14 16:21:32 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts.js";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        alt={contacts[0].alt}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        alt={contacts[1].alt}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        alt={contacts[2].alt}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
